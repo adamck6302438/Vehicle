6ee7bd649b96e5fd0dfb352411d99486bc70f7bf
Ray Kang <adamck6302438@gmail.com>
2019-05-05 18:55:55 -0400

    1st commit

--- Vehicles/Car.h
+++ Vehicles/Car.h
@@ -12,6 +12,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Car : Vehicle
 
+@property (nonatomic, assign) BOOL isConvertible;
+@property (nonatomic, assign) BOOL isHatchback;
+@property (nonatomic, assign) BOOL hasSunroof;
+@property (nonatomic, assign) NSInteger numberOfDoors;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- Vehicles/Vehicle.m
+++ Vehicles/Vehicle.m
@@ -8,6 +8,67 @@
 
 #import "Vehicle.h"
 
+@interface Vehicle() {
+    NSString *_brandName;
+}
+@end
+
 @implementation Vehicle
+
+//Setter method
+-(void)setBrandName:(NSString *)brandName
+{
+    _brandName = [brandName copy];
+}
+
+//Getter method
+-(NSString *)brandName
+{
+    return _brandName;
+}
+
+-(NSString *)goForward
+{
+    return nil;
+}
+
+-(NSString *)goBackward
+{
+    return nil;
+}
+
+-(NSString *)stopMoving
+{
+    return nil;
+}
+
+-(NSString *)turn:(NSInteger)degrees
+{
+    //Since there are only 360 degrees in a circle, calculate what a single turn would be.
+    NSInteger degreesInACircle = 360;
+    
+    if (degrees > degreesInACircle || degrees < -degreesInACircle) {
+        //The % operator returns the remainder after dividing.
+        degrees = degrees % degreesInACircle;
+    }
+    
+    return [NSString stringWithFormat:@"Turn %d degrees.", degrees];
+}
+
+-(NSString *)changeGears:(NSString *)newGearName
+{
+    return [NSString stringWithFormat:@"Put %@ into %@ gear.", self.modelName, newGearName];
+}
+
+-(NSString *)makeNoise
+{
+    return nil;
+}
+
+#pragma mark - Convenience Methods
+-(NSString *)vehicleTitleString
+{
+    return [NSString stringWithFormat:@"%d %@ %@", self.modelYear, self.brandName, self.modelName];
+}
 
 @end
