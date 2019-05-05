2d480ef7e7bf119c253535f568b08868c9643c97
Ray Kang <adamck6302438@gmail.com>
2019-05-05 18:53:10 -0400

    first commit

---
+++ Vehicles/Car.h
@@ -0,0 +1,17 @@
+//
+//  Car.h
+//  Vehicles
+//
+//  Created by Frances ZiyiFan on 5/4/19.
+//  Copyright © 2019 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Car : Vehicle
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Vehicles/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  Vehicles
+//
+//  Created by Frances ZiyiFan on 5/4/19.
+//  Copyright © 2019 Designated Nerd Software. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
